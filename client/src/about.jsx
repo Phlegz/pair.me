965b50e012821521798f79a75dbfa5b6613b7ffc
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-17 19:49:35 +0000

    Testing react router

---
+++ client/src/about.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <h3>It renders!</h3>
+  </div>
+)
+
+export default About;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import App from './app.jsx'
-
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
