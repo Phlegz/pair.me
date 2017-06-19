1e4129f9c0e424676bb13ca8f1bae1928815a632
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-19 18:01:22 +0000

    Update rollback for users name and email column

--- server/db/migrations/20170618014013_users_name_email_nullable.js
+++ server/db/migrations/20170618014013_users_name_email_nullable.js
@@ -11,8 +11,6 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.alterTable('users', function(table){
-      table.string('name').notNullable().alter();
-      table.string('email').notNullable().alter();
       table.dropColumn('github_id');
     })
   ])
