564618a3f5dda1f85839d5a0305e5a4c531ed02b
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-19 20:45:17 +0000

    Add react route for challenge

---
+++ client/src/components/Challenge.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Challenge extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm in Challenge Page</h1>
+      </div>
+    );
+  }
+}
+export default Challenge;
