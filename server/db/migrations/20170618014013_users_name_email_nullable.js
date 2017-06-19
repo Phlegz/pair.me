d51bff68a46ad53e23e1f4d6ae25d1733c3fe56b
dkyw <derrellwong@gmail.com>
2017-06-19 18:16:43 +0000

    Change rollback command for users name and email

---
+++ server/db/migrations/20170618014013_users_name_email_nullable.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('users', function(table){
+      table.string('name').alter();
+      table.string('email').alter();
+      table.integer('github_id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.alterTable('users', function(table){
+      table.string('name')
+      table.string('email')
+      table.dropColumn('github_id');
+    })
+  ])
+};
