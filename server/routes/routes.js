4c5ed539cf3ba5c2a0b6aadd03a15d3489fb4768
dkyw <derrellwong@gmail.com>
2017-06-19 04:15:13 +0000

    added logout route, clears session

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,25 +7,34 @@ const passport = require('passport');
 
 module.exports = (knex, bundleGenerated) => {
 
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+      res.redirect('/')
+  }
+
   router.get('/', (req, res) => {
     res.render('index')
   })
 
   router.get('/auth/github',
-    passport.authenticate('github', { scope: [ 'user:email' ] }),
-    function(req, res){
-      // The request will be redirected to GitHub for authentication, so this
-      // function will not be called.
-  });
+    passport.authenticate('github', { scope: [ 'user:email' ]
+  }));
 
+  //successful authentication, redirect to dashboard
   router.get('/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/login' }),
     function(req, res) {
       res.redirect('/dashboard');
-  });
+    });
 
-  router.get('/dashboard', (req, res) => {
+  router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', {bundleGenerated :bundleGenerated } )
+  })
+
+  router.get('/logout', (req,res) => {
+    req.logOut();
+    req.session.destroy();
+    res.redirect('/');
   })
 
   // router.get("/", (req, res) => {
