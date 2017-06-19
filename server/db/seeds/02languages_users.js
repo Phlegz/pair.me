5668e9172a3b1824aae4907d393c828a3d15af9b
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-19 18:00:07 +0000

    Change user_id seeds

--- server/db/seeds/02languages_users.js
+++ server/db/seeds/02languages_users.js
@@ -2,10 +2,10 @@ exports.seed = function(knex, Promise) {
   return knex('languages_users').del()
     .then(function () {
       return Promise.all([
-        knex('languages_users').insert({id: 1, user_id: 1, language_id: 1}),
-        knex('languages_users').insert({id: 2, user_id: 2, language_id: 3}),
-        knex('languages_users').insert({id: 3, user_id: 3, language_id: 4}),
-        knex('languages_users').insert({id: 4, user_id: 4, language_id: 2})
+        knex('languages_users').insert({id: 1, user_id: 1001, language_id: 1}),
+        knex('languages_users').insert({id: 2, user_id: 1002, language_id: 3}),
+        knex('languages_users').insert({id: 3, user_id: 1003, language_id: 4}),
+        knex('languages_users').insert({id: 4, user_id: 1004, language_id: 2})
       ]);
     });
 };
--- server/db/seeds/08sessions.js
+++ server/db/seeds/08sessions.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('sessions').del()
     .then(function () {
       return Promise.all([
-        knex('sessions').insert({id: 1, user1_id: 1, user2_id: 2, challenge_id: 1}),
-        knex('sessions').insert({id: 2, user1_id: 3, user2_id: 4, challenge_id: 3}),
-        knex('sessions').insert({id: 3, user1_id: 2, user2_id: 3, challenge_id: 2})
+        knex('sessions').insert({id: 1, user1_id: 1001, user2_id: 1002, challenge_id: 1}),
+        knex('sessions').insert({id: 2, user1_id: 1003, user2_id: 1004, challenge_id: 3}),
+        knex('sessions').insert({id: 3, user1_id: 1002, user2_id: 1003, challenge_id: 2})
       ]);
     });
 };
