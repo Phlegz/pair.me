f4d436e5d8d4846319d89e172786920696736648
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-20 00:52:10 +0000

    Submit code value from Ace on Submit

--- client/src/components/Challenge.jsx
+++ client/src/components/Challenge.jsx
@@ -5,20 +5,35 @@ import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
-function onChange(newValue) {
-  console.log('change',newValue);
-}
-
 class Challenge extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { content: '' }
+  // }
+  submitCode(event){
+    event.preventDefault();
+    //to get the instance of the Editor through ace-editor name
+    //finally use getValue() function to get the contents
+    var editor = ace.edit('codeChallenges');
+    console.log(editor.getValue());
+  }
+  onChange(newValue) {
+    //console.log(newValue);
+  } 
   render() {
     return (
- <AceEditor
-    mode="javascript"
-    theme="monokai"
-    onChange={onChange}
-    name="UNIQUE_ID_OF_DIV"
-    editorProps={{$blockScrolling: true}}
-  />
+      <div>
+          <AceEditor
+            name="codeChallenges"
+            mode="javascript"
+            theme="monokai"
+            onChange={ this.onChange }
+            editorProps={{$blockScrolling: true}}
+            value="// Type your code here"
+            tabSize="2"
+          />
+          <input type='button' value='Submit' onClick={this.submitCode} />
+      </div>
     );
   }
 }
