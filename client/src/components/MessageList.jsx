aa59d69c7db5406cd61459b63d8e0164a4e242a6
dkyw <derrellwong@gmail.com>
2017-06-21 19:20:10 +0000

    pass messages as prop as map through each message

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -4,12 +4,19 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
   render () {
+    const message = this.props.messages.map((message, index) => {
+      return (
+        <Message
+          key = { index }
+          username = { message.username }
+          content = { message.content }
+        />
+        )
+    });
     return (
-      <div>
-        <Message />
-      </div>
+      <div id='Message-List'>{message}</div>
     )
   }
 }
 
-export default Message;
+export default MessageList;
