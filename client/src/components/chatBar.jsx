dc9d74ca15820fa46e8886365ab2df71b9a4f7e1
dkyw <derrellwong@gmail.com>
2017-06-21 21:26:18 +0000

    added event handlers to track form changes

--- client/src/components/chatBar.jsx
+++ client/src/components/chatBar.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // username: '',
+      content: ''
+    };
+    this.onMessage = this.onMessage.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  onMessage(event){
+    this.setState({
+      content: event.target.value
+    });
+  }
+
+  handleKeyPress(event){
+    if(event.key === 'Enter'){
+      this.props.onNewMessage(this.state.content);
+      this.setState({
+        content: ''
+      });
+    }
+  }
   render () {
     return (
       <footer className="chatbar">
-        <input className="chatbar-message" placeholder="type message here" />
+        <input className="chatbar-message"
+          placeholder="type message here"
+          onChange={this.onMessage}
+          onKeyPress={this.handleKeyPress}
+          value = { this.state.content }
+          />
       </footer>
     )
   }
