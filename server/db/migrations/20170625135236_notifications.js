33215c1ab4f88ce775899764d74cfc5ab92a6de4
farnaz <farnaz.rashidi@gmail.com>
2017-06-25 16:41:25 -0700

    Added the notification table to the db

---
+++ server/db/migrations/20170625135236_notifications.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notifications', (table) => {
+      table.increments('id');
+      table.integer('user_id');
+      table.foreign('user_id').references('users.id');
+      // table.integer('session_id');
+      // table.foreign('session_id').references('sessions.id');
+      table.boolean('initiator');
+      table.boolean('status'); //accepted,rejected,ignored
+      table.timestamps(true,true);
+    })
+};
+
+exports.down = function(knex, Promise) {
+   return knex.raw("drop table sessions_users cascade");
+};
