50d8239e3c06117d3fa51b103801cfb7116f2b9f
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-26 04:27:19 +0000

    Add notifications_users table to fix many-to-many relationships

--- server/db/migrations/20170625135236_notifications.js
+++ server/db/migrations/20170625135236_notifications.js
@@ -2,14 +2,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('notifications', (table) => {
       table.increments('id');
-      table.integer('user_id');
-      table.foreign('user_id').references('users.id');
-      table.boolean('initiator');
       table.string('status').defaultTo('pending'); //accepted,rejected,ignored
       table.timestamps(true,true);
     })
 };
 
 exports.down = function(knex, Promise) {
-   return knex.raw("drop table notifications cascade");
+  return knex.raw("drop table notifications cascade");
 };
---
+++ server/db/migrations/20170626032010_notifications_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notifications_users', (table) => {
+    table.increments('id');
+    table.integer('user_id');
+    table.foreign('user_id').references('users.id');
+    table.integer('notification_id');
+    table.foreign('notification_id').references('notifications.id');
+    table.boolean('initiator');
+    table.unique(['user_id', 'notification_id']);
+    table.unique(['notification_id', 'initiator']);
+    table.timestamps(true,true);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.raw("drop table notifications_users cascade");
+};
