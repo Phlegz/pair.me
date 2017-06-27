1409c9724ca1b7b5edecf9098e9e335021404483
dkyw <derrellwong@gmail.com>
2017-06-27 00:23:04 +0000

    verify who sent the message

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -4,12 +4,15 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
   render () {
+
     const message = this.props.messages.map((message, index) => {
+
       return (
         <Message
           key = { index }
           username = { message.username }
           content = { message.content }
+          fromMe = {this.props.user.github_username === message.username }
         />
         )
     });
