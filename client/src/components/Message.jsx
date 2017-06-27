b9bae6ce24b47c7c145fa64590c590fb4ac2f1ab
dkyw <derrellwong@gmail.com>
2017-06-27 00:22:40 +0000

    added from-me class if user sent the message

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+
+
   render () {
+    //add class if user sent the message
+    const fromMe = this.props.fromMe ?'from-me' :'';
+
     return(
       <main>
-        <div>
+        <div className={`message ${fromMe}`}>
           <span className="chatbar-username">{ this.props.username }</span>
           <span className="message-content">{ this.props.content }</span>
         </div>
@@ -12,5 +17,8 @@ class Message extends Component {
     )
   }
 }
+
+
+
 
 export default Message;
