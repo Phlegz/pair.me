01205d28b288712819f3349fc49baee9fce45b96
dkyw <derrellwong@gmail.com>
2017-06-27 02:39:52 +0000

    merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {} from '../styles/application.css';
 import {} from '../styles/profile.css';
+import {} from '../styles/challengeLayout.css';
 // import {} from '../styles/font-awesome-4.7.0/css/font-awesome.min.css'
 
 import Dashboard from './components/dashboard.jsx'
